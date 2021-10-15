3cdf7e6d0aedf520ded5b6bd46021e009807c9cd
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-10-16 01:01:30 +0530

    Helper code folder is added for java

---
+++ Boiler Plate Code For Java/Main1.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+
+// input() to take one line as an input from the input file
+// print() to print one line but will not provide new line at the end
+// println() to print one line and will provide new line at the end
+
+class Main1{
+
+    public static void solution() throws IOException{
+
+    }
+       
+    public static void main(String[] args) throws IOException{
+
+        // input() to take one line as an input from the input file
+        // print() to print one line but will not provide new line at the end
+        // println() to print one line and will provide new line at the end
+        
+        solution(); // solution() is for you to work
+        free(); // free() is for flushing and closing the streams        
+    }
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static {
+        try{
+            br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("output.txt")));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static String input() throws IOException{
+        return br.readLine();
+    }
+    public static void print(String line) throws IOException{
+        bw.write(line);
+    }
+    public static void println(String line) throws IOException{
+        bw.write(line);
+        bw.newLine();
+    }
+    public static void free() throws IOException{
+        br.close();
+        bw.close();
+    }
+}

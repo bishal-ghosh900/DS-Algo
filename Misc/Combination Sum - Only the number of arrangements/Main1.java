67fbca60beb129493f895b835078749a103e8ae0
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-12-26 01:48:05 +0530

    Combination Sum - Only the number of arrangements

---
+++ Misc/Combination Sum - Only the number of arrangements/Main1.java
@@ -0,0 +1,26 @@
+/**
+ * Main8
+ */
+public class Main8 {
+
+    static int res = 0;
+    public static void main(String[] args) {
+        int arr[] = {15, 20, 17, 45, 11};
+        int sum = 55;
+
+        getAllCombination(arr, sum, 0);
+        System.out.println(res);
+    }
+
+    private static void getAllCombination(int[] arr, int sum, int index) {
+
+        if(sum == 0){
+            res++;
+        }
+        for(int i = index; i < arr.length; i++){
+            if(sum - arr[i] >= 0){
+                getAllCombination(arr, sum - arr[i], i);
+            }
+        }
+    }
+}
